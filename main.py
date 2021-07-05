dc09ab83c9afc8022f83c87a6cf12060103e312c
Gerlinde1997 <60713279+Gerlinde1997@users.noreply.github.com>
2021-07-06 00:41:48 +0200

    add drawings

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 
 """
 Make a Cake
-
 Command line tool for baking your own virtual birthday cake
+
+Copyright (C) 2021 Gerlinde van Ginkel
+SPDX-License-Identifier: GPL-3.0-or-later
 """
 
 __author__ = "Gerlinde van Ginkel"
@@ -11,21 +13,89 @@ __version__ = "2021.v1"
 
 import sys
 import argparse
-import turtle
+from turtle import *
+from shapes import Shapes
 
-FLAVOURS = {"strawberry": (255, 59, 59)}
+FLAVOURS = {"strawberry": "#D93447", 
+    "vanille": "#F5EBBC", 
+    "chocolate": "#301D01",
+    "lemon": "#FCFF9E",
+    "raspberry": "#FFABDF",
+    "pistachio": "#D5FFAB"}
+
+ICING = {"sugar": "#F0EDE6", "caramel": "#B57F00", "chocolate": "#291900"}
+
+def set_window():
+    window = Screen()
+    window.title("Congratulations!")
+    window.screensize(500, 500)
+    window.setworldcoordinates(0, 100, 100, 0)
+    window.tracer(0)
+    window.bgcolor("#9EECF0")
+
+    return window
+
+
+def draw_wick(pencil):
+    pencil.penup()
+    pencil.goto(50, 12)
+    pencil.pensize(2)
+    pencil.color("#000000")
+    pencil.pendown()    
+    pencil.forward(4)
+    pencil.getscreen().update()
+
 
 def main(args):
 
+    # argparse given 2 flavours and  1 icing (3 args)
     #set pencil
-    pencil = turtle.Turtle()
+    pencil = Turtle()
     pencil.speed(0)
     pencil.hideturtle()
 
     #set screen/window
-    window = turtle.Screen()
-    window.bgcolor("orange")
+    window = set_window()
+
+    # plate
+    plate = Shapes(pencil, "#E4F6F7", 10, 90)
+    plate.draw_rectangle(80, 4)
+
+    # bottom layer
+    bottom_layer = Shapes(pencil, FLAVOURS["pistachio"], 20, 70)
+    bottom_layer.draw_rectangle(60, 19.9)
+    bottom_icing = Shapes(pencil, ICING["sugar"], 19, 70)
+    bottom_icing.draw_icing(80)
+    
+    # middle layer
+    middle_layer = Shapes(pencil, FLAVOURS["raspberry"], 30, 50)
+    middle_layer.draw_rectangle(40, 19.9)
+    middle_icing = Shapes(pencil, ICING["caramel"], 29, 50)
+    middle_icing.draw_icing(70)
+
+    # upper layer
+    upper_layer = Shapes(pencil, FLAVOURS["chocolate"], 40, 30)
+    upper_layer.draw_rectangle(20, 19.9)
+
+    # candle
+    wax = Shapes(pencil, "red", 48, 16)
+    wax.draw_rectangle(4, 14)
+    flame = Shapes(pencil, "orange", 52, 12)
+    flame.draw_flame()
+    draw_wick(pencil)
+
+    # upper icing
+    upper_icing = Shapes(pencil, ICING["chocolate"], 39, 30)
+    upper_icing.draw_icing(60)
+
+    cream = Shapes(pencil, "white", 20, 82)
+    cream.draw_cream(80)
+    cream = Shapes(pencil, "white", 30, 62)
+    cream.draw_cream(70)
+    cream = Shapes(pencil, "white", 40, 42)
+    cream.draw_cream(60)
     window.exitonclick()
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
--- shapes.py
+++ shapes.py
@@ -2,15 +2,15 @@
 
 """
 Make a Cake
-Command line tool for baking your own virtual birthday cake
-
 Python module with the class Shapes
+
+Copyright (C) 2021 Gerlinde van Ginkel
+SPDX-License-Identifier: GPL-3.0-or-later
 """
 
 __author__ = "Gerlinde van Ginkel"
 __version__ = "2021.v1"
 
-import sys
 import turtle
 import math
 
@@ -21,12 +21,83 @@ class Shapes:
     def __init__(self, turtle, color, x, y):
         self.pencil = turtle
         self.color = color
-        self.target = (x, y)
+        self.x = x
+        self.y = y
     
-    def draw_cirkel(self):
-        return 0
+    # rectangle for layers (width, height)
+    # icing (width, height)
+    # whipped cream (width, height)
+    # candle -> rectangle, line and flame
+    
+    def draw_rectangle(self, width, height):
+        pencil = self.pencil
+        pencil.hideturtle()
+        pencil.penup()
+        pencil.color(self.color)
+        pencil.fillcolor(self.color)
+        pencil.goto(self.x, self.y)
+        pencil.pendown()
+        pencil.begin_fill()
 
+        for side in range(2):
+            pencil.forward(width)
+            pencil.left(90)
+            pencil.forward(height)
+            pencil.left(90)
+
+        pencil.end_fill()
+        pencil.setheading(0)  
+        pencil.getscreen().update()
     
+
+    def draw_icing(self, ru):
+        pencil = self.pencil
+        pencil.penup()
+        pencil.color(self.color)
+        pencil.fillcolor(self.color)
+        pencil.goto(self.x, self.y)
+        pencil.pendown()
+        pencil.begin_fill()
+
+        for x in range(self.x, ru+2):
+            pencil.goto(x, self.y+5 + 2*math.cos((x/10)*2*math.pi))
+        
+        pencil.goto(ru+1, self.y)
+        pencil.goto(self.x, self.y)
+        pencil.end_fill()
+        pencil.getscreen().update()
+    
+    def draw_cream(self, rux):
+        pencil = self.pencil
+        pencil.penup()
+        pencil.color(self.color)
+        pencil.pensize(20)
+        pencil.goto(self.x, self.y)
+        pencil.pendown()
+
+        for x in range(self.x, rux+1):
+            pencil.goto(x, self.y + math.sin((x/10)*2*math.pi))
+        
+        pencil.end_fill()
+        pencil.getscreen().update()
+    
+
+    def draw_flame(self):
+        size = 2
+        pencil = self.pencil
+        pencil.penup()
+        pencil.color(self.color)
+        pencil.goto(self.x, self.y)
+        pencil.pendown()
+        pencil.seth(-270)
+        pencil.begin_fill()
+        pencil.circle(size, 180)
+        pencil.circle(2*size, 45)
+        pencil.circle(size*0.586, 90)
+        pencil.circle(2*size, 45)
+        pencil.end_fill()
+        pencil.getscreen().update()
+
 
 if __name__ == "__main__":
-    SHAPES = Shapes()
+    SHAPES = Shapes(turtle.Turtle(), "orange", 50, 50)
